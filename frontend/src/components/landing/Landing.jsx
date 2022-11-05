36f1f002654cc3141101a499faf80cac2f3a7185
NotSanti <santiluna03@outlook.com>
2022-11-05 16:40:26 -0400

    created landing page

---
+++ frontend/src/components/landing/Landing.jsx
@@ -0,0 +1,22 @@
+import "./landing.css";
+import * as React from "react";
+
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
+import ResponsiveAppBar from "./ResponsiveAppBar";
+
+import Button from "@mui/material/Button";
+import { Card, AppBar } from "@mui/material";
+
+export default function Landing() {
+  return (
+    <>
+      <ResponsiveAppBar></ResponsiveAppBar>
+
+      <Card variant="outlined">Posts go here</Card>
+    </>
+  );
+}
