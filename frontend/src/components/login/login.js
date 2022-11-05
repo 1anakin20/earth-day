8d210aca3e899e6e4cd2d6428e9793198aa8d94d
1anakin20 <tomasdaniel09@hotmail.com>
2022-11-04 21:17:03 -0400

    Add login webpage

---
+++ frontend/src/components/login/login.js
@@ -0,0 +1,27 @@
+import "./Login.css";
+
+function Login() {
+    return (<div className="login">
+        <div className="login__container">
+            <h1><span className={"black"}>Glean</span><span className={"green"}>ful</span></h1>
+            <p>Don't have an account?</p>
+            <Link className={"register"} to={"/register"}>Register</Link>
+            <hr className={"register__separation"}/>
+            <p className={"label"}>Email</p>
+            <input
+                type="email"
+                placeholder="Email"/>
+            <p className={"label"}>Password</p>
+            <input
+                type="password"
+                placeholder="Password"/>
+            <button
+                className="login__btn">
+                Login
+            </button>
+            <Link to={'/reset'}>Forgot password?</Link>
+        </div>
+    </div>);
+}
+
+export default Login;
