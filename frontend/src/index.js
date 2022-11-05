b5ebac86b951bcb42978345ccd4ed4839cc22017
Timothy Chan <timothy.chan2@mail.mcgill.ca>
2022-11-04 21:12:53 -0400

    Add ability to handle routes that don't exist to divert back to the home page

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Navigate
+} from 'react-router-dom';
 import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App />} />
+        <Route path='*' element={<Navigate to='/' />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
