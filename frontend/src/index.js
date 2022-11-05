63d3b2d15c5524dff51100daef17011609c09609
1anakin20 <tomasdaniel09@hotmail.com>
2022-11-04 21:12:02 -0400

    Add login route

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,22 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {
-  BrowserRouter,
-  Routes,
-  Route,
-  Navigate
+    BrowserRouter, Routes, Route, Navigate
 } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import Login from './components/login/login';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+root.render(<React.StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<App />} />
-        <Route path='*' element={<Navigate to='/' />} />
-      </Routes>
+        <Routes>
+            <Route path='/' element={<App/>}/>
+            <Route path='*' element={<Navigate to='/'/>}/>
+            <Route path='/login' element={<Login/>}/>
+        </Routes>
     </BrowserRouter>
-  </React.StrictMode>
-);
+</React.StrictMode>);
