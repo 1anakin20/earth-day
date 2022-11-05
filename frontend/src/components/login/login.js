56648aaa35af86582110044d3fa53867fd0bc5fb
1anakin20 <tomasdaniel09@hotmail.com>
2022-11-05 06:29:53 -0400

    Change login form to use material UI

--- frontend/src/components/login/login.js
+++ frontend/src/components/login/login.js
@@ -1,25 +1,36 @@
 import "./Login.css";
-import {Link} from "react-router-dom";
+import Button from '@mui/material/Button';
+import Link from "@mui/material/Link";
+import Divider from "@mui/material/Divider";
+import Chip from "@mui/material/Chip";
+import TextField from "@mui/material/TextField";
 
 function Login() {
     return (<div className="login">
         <div className="login__container">
             <h1><span className={"black"}>Glean</span><span className={"green"}>ful</span></h1>
             <p>Don't have an account?</p>
-            <Link className={"register"} to={"/register"}>Register</Link>
-            <hr className={"register__separation"}/>
-            <p className={"label"}>Email</p>
-            <input
+            <Link href="#" underline="none">Register</Link>
+            {/*<hr className={"register__separation"}/>*/}
+            <Divider variant={"middle"} className={"register__separation"}>
+                <Chip label={"Or login"} color={"default"}/>
+            </Divider>
+            <TextField
                 type="email"
-                placeholder="Email"/>
-            <p className={"label"}>Password</p>
-            <input
+                placeholder="Email"
+                autoComplete="email"
+                focused/>
+            <TextField
+                id="outlined-password-input"
+                label="Password"
                 type="password"
-                placeholder="Password"/>
-            <button
+                autoComplete="current-password"
+                variant="outlined"/>
+            <Button
+                variant={"contained"}
                 className="login__btn">
                 Login
-            </button>
+            </Button>
             <Link to={'/reset'}>Forgot password?</Link>
         </div>
     </div>);
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,6 +6,10 @@ import {
 import './index.css';
 import App from './App';
 import Login from './components/login/login';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<React.StrictMode>
