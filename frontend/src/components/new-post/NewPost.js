2891b1bc900f4ec92e5481c308eb1b5bfc9c6b78
1anakin20 <tomasdaniel09@hotmail.com>
2022-11-05 15:13:20 -0400

    Integrate nav bar to new post

--- frontend/src/components/new-post/NewPost.js
+++ frontend/src/components/new-post/NewPost.js
@@ -2,6 +2,7 @@ import {useState} from "react";
 import Grid from "@mui/material/Grid";
 import {Button, TextareaAutosize, TextField} from "@mui/material";
 import "./NewPost.css";
+import ResponsiveAppBar from "../ResponsiveAppBar";
 
 const defaultValues = {
     farm: "",
@@ -30,94 +31,97 @@ const NewPost = () => {
         console.log(formValues);
     };
     return (
-        <form onSubmit={handleSubmit} id={"form"}>
-            <Grid className={"form__grid"} container alignItems="center" justify="center" direction="column">
-                <Grid item className={"fieldset"}>
-                    <p>Farm</p>
-                    <TextField
-                        id="farm"
-                        name="farm"
-                        label="farm"
-                        type="text"
-                        value={formValues.name}
-                        onChange={handleInputChange}
-                        required
-                        variant={"outlined"}
-                    />
+        <div>
+            <ResponsiveAppBar />
+            <form onSubmit={handleSubmit} id={"form"}>
+                <Grid className={"form__grid"} container alignItems="center" justify="center" direction="column">
+                    <Grid item className={"fieldset"}>
+                        <p>Farm</p>
+                        <TextField
+                            id="farm"
+                            name="farm"
+                            label="farm"
+                            type="text"
+                            value={formValues.name}
+                            onChange={handleInputChange}
+                            required
+                            variant={"outlined"}
+                        />
+                    </Grid>
+                    <Grid item className={"fieldset"}>
+                        <p>Crop Type</p>
+                        <TextField
+                            id="cropType"
+                            name="cropType"
+                            label="crop type"
+                            type="text"
+                            value={formValues.cropType}
+                            onChange={handleInputChange}
+                            required
+                        />
+                    </Grid>
+                    <Grid item className={"fieldset"}>
+                        <p>Description</p>
+                        <TextareaAutosize
+                            minRows={10}
+                            id="description"
+                            name="description"
+                            label="description"
+                            type="text"
+                            placeholder={"Enter a description about this opportunity"}
+                            value={formValues.description}
+                            onChange={handleInputChange}
+                        />
+                    </Grid>
+                    <Grid item className={"fieldset"}>
+                        <p>Date</p>
+                        <TextField
+                            id="date"
+                            name="date"
+                            label="date"
+                            type="date"
+                            value={formValues.date}
+                            onChange={handleInputChange}
+                            required/>
+                    </Grid>
+                    <Grid item className={"fieldset"}>
+                        <p>Address</p>
+                        <TextField
+                            id="address"
+                            name="address"
+                            label="address"
+                            type="text"
+                            value={formValues.address}
+                            onChange={handleInputChange}
+                            required/>
+                    </Grid>
+                    <Grid item className={"fieldset"}>
+                        <p>Food Bank</p>
+                        <TextField
+                            id="foodBank"
+                            name="foodBank"
+                            label="food bank"
+                            type="text"
+                            value={formValues.foodBank}
+                            onChange={handleInputChange}/>
+                    </Grid>
+                    <Grid item className={"fieldset"}>
+                        <p>Capacity</p>
+                        <TextField
+                            id="capacity"
+                            name="capacity"
+                            label="capacity"
+                            type="number"
+                            value={formValues.capacity}
+                            onChange={handleInputChange}
+                            required/>
+                    </Grid>
+                    <Button variant="contained" color="primary" type="submit" id={"post"}>
+                        Post
+                    </Button>
                 </Grid>
-                <Grid item className={"fieldset"}>
-                    <p>Crop Type</p>
-                    <TextField
-                        id="cropType"
-                        name="cropType"
-                        label="crop type"
-                        type="text"
-                        value={formValues.cropType}
-                        onChange={handleInputChange}
-                        required
-                    />
-                </Grid>
-                <Grid item className={"fieldset"}>
-                    <p>Description</p>
-                    <TextareaAutosize
-                        minRows={10}
-                        id="description"
-                        name="description"
-                        label="description"
-                        type="text"
-                        placeholder={"Enter a description about this opportunity"}
-                        value={formValues.description}
-                        onChange={handleInputChange}
-                    />
-                </Grid>
-                <Grid item className={"fieldset"}>
-                    <p>Date</p>
-                    <TextField
-                        id="date"
-                        name="date"
-                        label="date"
-                        type="date"
-                        value={formValues.date}
-                        onChange={handleInputChange}
-                        required/>
-                </Grid>
-                <Grid item className={"fieldset"}>
-                    <p>Address</p>
-                    <TextField
-                        id="address"
-                        name="address"
-                        label="address"
-                        type="text"
-                        value={formValues.address}
-                        onChange={handleInputChange}
-                        required/>
-                </Grid>
-                <Grid item className={"fieldset"}>
-                    <p>Food Bank</p>
-                    <TextField
-                        id="foodBank"
-                        name="foodBank"
-                        label="food bank"
-                        type="text"
-                        value={formValues.foodBank}
-                        onChange={handleInputChange}/>
-                </Grid>
-                <Grid item className={"fieldset"}>
-                    <p>Capacity</p>
-                    <TextField
-                        id="capacity"
-                        name="capacity"
-                        label="capacity"
-                        type="number"
-                        value={formValues.capacity}
-                        onChange={handleInputChange}
-                        required/>
-                </Grid>
-                <Button variant="contained" color="primary" type="submit" id={"post"}>
-                    Post
-                </Button>
-            </Grid>
-        </form>
+            </form>
+        </div>
     );
 };
 export default NewPost;
