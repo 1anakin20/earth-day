ead4228681ddfb68055dfa129a1c68befa092060
Veronika Pontolillo <veronika.pontolillo@dawsoncollege.qc.ca>
2022-11-06 10:10:22 -0500

    Modified css styling

--- frontend/src/components/post_details/PostDetails.js
+++ frontend/src/components/post_details/PostDetails.js
@@ -1,37 +1,66 @@
 import "./PostDetails.css";
 import Button from '@mui/material/Button';
-import Link from "@mui/material/Link";
-import Divider from "@mui/material/Divider";
-import Chip from "@mui/material/Chip";
-import TextField from "@mui/material/TextField";
-import {useState} from "react";
 import ResponsiveAppBar from "../ResponsiveAppBar";
+import React from "react";
+import { FaArrowLeft } from 'react-icons/fa';
+import { useNavigate } from "react-router-dom";
 
 function PostDetails() {
+
+    // when back arrow is clicked, user is redirected to the home page
+    let navigate = useNavigate(); 
+    const homeRoute = () => { 
+        let path = `..\.`; 
+        navigate(path);
+    };
 
     return (
         <div className="details__root">
             <div className="details__content">
                 <ResponsiveAppBar/>
-                <div className="posts_details">
-                    <div className="details__container">
-                      <h1>Post Details</h1>
-                        <p>Farm: </p>
-                        <p>Crop Type: </p>
-                        <p>Description: </p>
-                        <p>Date: </p>
-                        <p>Address: </p>
-                        <p>Food Bank: </p>
-                        <p>Capacity: </p>
-                        <Button
-                            variant={"contained"}
-                            className="back__btn">
-                            <a href='/'>Back</a>
-                        </Button>
-                    </div>
-                </div>
+
+        <div className="posts__details">
+            <div className="details__container">
+              <h1>Post Details</h1>
+              <div className="details">
+                  <p>Farm: </p>
+                  <p>Quinn's Farm</p>
+                  </div>
+                  <div className="details">
+                  <p>Crop Type: </p>
+                  <p>Apples</p>
+                  </div>
+                  <div className="details">
+                  <p>Description: </p>
+                  <p>Look at all them apples</p>
+                  </div>
+                  <div className="details">
+                  <p>Date: </p>
+                  <p>06-11-2022</p>
+                  </div>
+                  <div className="details">
+                  <p>Address: </p>
+                  <p>1234 road</p>
+                  </div>
+                  <div className="details">
+                  <p>Food Bank: </p>
+                  <p>westmount</p>
+                  </div>
+                  <div className="details">
+                  <p>Capacity: </p>
+                  <p>8</p>
+                  </div>
+                
+                  <Button
+                      variant={"contained"}
+                      className="back__btn"
+                      onClick={homeRoute}>
+                      Back
+                  </Button>
             </div>
-        </div>);
+      </div>
+      </div>
+  </div>);
 }
 
 export default PostDetails;
