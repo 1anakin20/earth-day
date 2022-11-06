d6546e8c33e1d69bd53721b7aad4a9a798e6700b
Veronika Pontolillo <veronika.pontolillo@dawsoncollege.qc.ca>
2022-11-05 20:29:31 -0400

    Add base code for detailed posts

---
+++ frontend/src/components/post_details/PostDetails.js
@@ -0,0 +1,24 @@
+import {useState} from "react";
+import Grid from "@mui/material/Grid";
+import {Button, TextareaAutosize, TextField} from "@mui/material";
+import "./PostDetails.css";
+
+const defaultValues = {
+  farm: "",
+  cropType: "",
+  description: "",
+  date: null,
+  address: "",
+  foodBank: "",
+  capacity: 0,
+};
+
+const PostDetails = () => {
+
+
+return (
+
+  );
+};
+
+export default PostDetails;
