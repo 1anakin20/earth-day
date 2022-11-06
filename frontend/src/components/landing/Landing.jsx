8deb0bc3c897382d833ea581c1bcc9c6e1058afe
NotSanti <santiluna03@outlook.com>
2022-11-06 01:17:33 -0400

    remove useless imports

--- frontend/src/components/landing/Landing.jsx
+++ frontend/src/components/landing/Landing.jsx
@@ -1,6 +1,5 @@
 import "./landing.css";
 import * as React from "react";
-import NewPost from "../new-post/NewPost";
 
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
@@ -8,7 +7,6 @@ import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 
 import ResponsiveAppBar from "./ResponsiveAppBar";
-import { Card } from "@mui/material";
 import Post from "../post/Post";
 
 export default function Landing() {
