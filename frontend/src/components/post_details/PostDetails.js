5f339ec93f7e152a13796a3bcbe9c7154888fb9d
Veronika Pontolillo <veronika.pontolillo@dawsoncollege.qc.ca>
2022-11-05 23:20:45 -0400

    Added base for post details

--- frontend/src/components/post_details/PostDetails.js
+++ frontend/src/components/post_details/PostDetails.js
@@ -1,24 +1,37 @@
-import {useState} from "react";
-import Grid from "@mui/material/Grid";
-import {Button, TextareaAutosize, TextField} from "@mui/material";
 import "./PostDetails.css";
+import Button from '@mui/material/Button';
+import Link from "@mui/material/Link";
+import Divider from "@mui/material/Divider";
+import Chip from "@mui/material/Chip";
+import TextField from "@mui/material/TextField";
+import {useState} from "react";
+import ResponsiveAppBar from "../ResponsiveAppBar";
 
-const defaultValues = {
-  farm: "",
-  cropType: "",
-  description: "",
-  date: null,
-  address: "",
-  foodBank: "",
-  capacity: 0,
-};
+function PostDetails() {
 
-const PostDetails = () => {
-
-
-return (
-
-  );
-};
+    return (
+        <div className="details__root">
+            <div className="details__content">
+                <ResponsiveAppBar/>
+                <div className="posts_details">
+                    <div className="details__container">
+                      <h1>Post Details</h1>
+                        <p>Farm: </p>
+                        <p>Crop Type: </p>
+                        <p>Description: </p>
+                        <p>Date: </p>
+                        <p>Address: </p>
+                        <p>Food Bank: </p>
+                        <p>Capacity: </p>
+                        <Button
+                            variant={"contained"}
+                            className="back__btn">
+                            <a href='/'>Back</a>
+                        </Button>
+                    </div>
+                </div>
+            </div>
+        </div>);
+}
 
 export default PostDetails;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -12,6 +12,7 @@ import App from './App';
 import Register from './components/register/Register';
 import NewPost from "./components/new-post/NewPost";
 import Login from './components/login/login';
+import PostDetails from './components/post_details/PostDetails';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<React.StrictMode>
@@ -20,6 +21,7 @@ root.render(<React.StrictMode>
         <Route path='/login' element={<Login/>}/>
         <Route path='register' element={<Register />} />
         <Route path='/new_post' element={<NewPost />}/>
+        <Route path='/post_details' element={<PostDetails />}/>
         <Route path='/' element={<App/>}/>
         <Route path='*' element={<Navigate to='/'/>}/>
       </Routes>
