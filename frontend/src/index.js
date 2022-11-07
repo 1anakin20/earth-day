19fb9fe1731e23b813b310ae7fb31304b12c5d13
Timothy Chan <timothy.chan2@mail.mcgill.ca>
2022-11-06 21:13:28 -0500

    Remove the commented out code since they are no longer needed for this file

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,27 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import {
-//     BrowserRouter,
-//     Routes,
-//     Route,
-//     Navigate
-// } from 'react-router-dom';
+
 import './index.css';
 
 import App from './App';
-// import Register from './components/register/Register';
-// import NewPost from "./components/new-post/NewPost";
-// import Login from './components/login/login';
+
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-// import Profile from './components/profile/profile';
-// import PostDetails from './components/post_details/PostDetails';
-// import {QueryClient, QueryClientProvider} from "react-query";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-// const queryClient = new QueryClient();
+
 root.render(
     <React.StrictMode>
         <App/> 
