d5cf2b9f36d8437661d4f99cb1d24c71071110ce
daniel ji <danielji2008@gmail.com>
2022-11-06 20:07:18 -0500

    create posts, add ids to post, get/remove ids

---
+++ index.js
@@ -0,0 +1,181 @@
+import { initializeApp } from 'firebase/app';
+import { getDatabase, ref, set, child, update, onValue, push, remove, get} from 'firebase/database';  
+
+//const firebaseConfig = {
+//
+
+//};
+
+const app = initializeApp(firebaseConfig);
+
+//===========================================================================================
+// 1. add records to database
+//===========================================================================================
+
+//------------------------------add gleaner---------------------------------------------//
+
+function writeGleanerData(firstName, lastName, address, city, province, email, phone, groupSize, availability, password) {
+    const db = getDatabase(app);
+    // const reference = ref(db, 'gleaner/' + userId); 
+
+    const ListRef = ref(db, 'gleaner');
+    const newRef = push(ListRef);
+    
+    set( newRef, {
+         firstName: firstName,
+         lastName: lastName, 
+         address: address, 
+         city: city,
+         province: province,         
+         email: email, 
+         phone: phone, 
+         groupSize: groupSize, 
+         availability: availability, 
+         password: password
+                  
+               
+        });
+  }  
+
+//   writeGleanerData('jack', 'wang','123 montreal','montreal','quebec' ,'111_jack@gmail.com', '5147897894', '1450', 'Nov. 7', 'xy');  
+
+
+
+//------------------------------add farmer---------------------------------------------//
+
+function writeFarmerData(farmName, firstName, lastName, address, city, province, email, phone, availability, password) {
+    const db = getDatabase(app);
+    // const reference = ref(db, 'gleaner/' + userId); 
+
+    const ListRef = ref(db, 'farm');
+    const newRef = push(ListRef);
+  
+      set( newRef, {
+         farmName: farmName,
+         firstName: firstName,
+         lastName: lastName, 
+         address: address, 
+         city: city,
+         province: province,         
+         email: email, 
+         phone: phone, 
+         availability: availability, 
+         password: password
+                  
+               
+        });
+  }
+
+//   writeFarmerData('abc farm','jack', 'wang','123 montreal','montreal','quebec' ,'111_jack@gmail.com', '5147897894', 'Nov. 7', 'xy');  
+  
+
+
+
+//------------------------------add food bank---------------------------------------------//
+
+function writeFoodbankData(FoodbankName, firstName, lastName, address, city, province, email, phone, availability, password) {
+    const db = getDatabase(app);
+    // const reference = ref(db, 'gleaner/' + userId); 
+
+    const ListRef = ref(db, 'foodbank');
+    const newRef = push(ListRef);
+  
+  
+    set( newRef, {
+         FoodbankName:FoodbankName,
+         firstName: firstName,
+         lastName: lastName, 
+         address: address, 
+         city: city,
+         province: province,         
+         email: email, 
+         phone: phone, 
+         availability: availability, 
+         password: password
+                 
+               
+        });
+  } 
+  
+//  writeFoodbankData('sdf bank','jack', 'wang','123 montreal','montreal','quebec' ,'111_jack@gmail.com', '5147897894', 'Nov. 7', 'xy');  
+
+
+
+
+//===========================================================================================
+// 2. create posts (gleaning post, inventory post, storage post)
+//===========================================================================================
+
+  //create gleaning post---------------------------------------------//
+
+function createGleaningPost(farmId, description, gleanerList) {
+  const db = getDatabase();
+  const postListRef = ref(db, 'posts');
+  const newPostRef = push(postListRef);
+  set(newPostRef, {
+      farmId: farmId,
+      description: description,
+      gleanerList: gleanerList
+   
+  });
+}
+
+// createGleaningPost('1', 'apple, good', '')
+
+ 
+//===========================================================================================
+// 3. add gleaners(food banks, farms) to related post lists
+//===========================================================================================
+
+//add gleaners to post---------------------------------------------//
+
+function addGleanerToPost(gleanerId, postId) {
+  const db = getDatabase();
+  const postListRef = ref(db, 'posts/' + postId + '/gleanerList');
+  const newPostRef = push(postListRef);
+  set(newPostRef, {
+      gleanerId: gleanerId,
+   
+  });
+}
+
+
+// addGleanerToPost('777', '-NGEMuppk2GPCDYD22je')
+
+
+
+// to create inventory post from farms(which can be seen by food bank), and add food banks to the list 
+// to create storage post from food bank(which can be seen by farms) , and add farms to the list
+
+
+
+
+// select key, value----------/   -NGA-78iLQRYeoNUSeeH
+// get gleaner id
+
+const dbRef = ref(getDatabase());
+// get(child(dbRef, 'gleaner/-NGA-78iLQRYeoNUSeeH')).then((snapshot) => {
+  get(child(dbRef, 'gleaner/')).then((snapshot) => {
+  if (snapshot.exists()) {
+    console.log(snapshot.val());
+  } else {
+    console.log("No data available");
+  }
+}).catch((error) => {
+  console.error(error);
+});
+
+
+
+//------------------------------remove record---------------------------------------------//
+
+function deleteDate(){
+	remove(ref (db, "gleaner/" + gleanId))
+    .then( ()=>{
+        alert("data removed successfully");
+    })
+    .catch((error) =>{
+        alert("unsuccessful, error" + error);
+    });
+}
+
