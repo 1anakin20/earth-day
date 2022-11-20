1193e1ceece81885aad9b56a794ec2a51dab6b05
Timothy Chan <timothy.chan2@mail.mcgill.ca>
2022-11-20 00:40:19 -0500

    Fix incorrect indentation of the code

--- frontend/src/components/profile/profile.js
+++ frontend/src/components/profile/profile.js
@@ -50,111 +50,111 @@ function Profile(props) {
     };
 
     return (
-        <div className="profile__root">
-            <div className="profile__content">
-                <ResponsiveAppBar
-                    user={user}
-                />
+    <div className="profile__root">
+        <div className="profile__content">
+            <ResponsiveAppBar
+                user={user}
+            />
 
-        <div className="profile">
-            <div className="profile__container">
-                <Button id="back_btn" onClick={routeChange}>
-                <FaArrowLeft />
-                </Button>
-                <h1 id="profile_tag"> Profile </h1>
-                <img src={defaultProfilePic} alt="Profile Picture" id="profilePic" />
-                    
-                <Button onClick={handleImageUpload}>
-                    Change Profile Picture
-                </Button>
+            <div className="profile">
+                <div className="profile__container">
+                    <Button id="back_btn" onClick={routeChange}>
+                    <FaArrowLeft />
+                    </Button>
+                    <h1 id="profile_tag"> Profile </h1>
+                    <img src={defaultProfilePic} alt="Profile Picture" id="profilePic" />
+                        
+                    <Button onClick={handleImageUpload}>
+                        Change Profile Picture
+                    </Button>
 
-                <Button
-                    variant={"contained"}
-                    color="error"
-                    className="modify__btn"
-                    onClick={signOut}>
-                    Log Out
-                </Button>
+                    <Button
+                        variant={"contained"}
+                        color="error"
+                        className="modify__btn"
+                        onClick={signOut}>
+                        Log Out
+                    </Button>
 
-                {/* Some hardcoded values still left to update */}
-                <div className="info">
-                    <p> <b>Account Type </b></p>
-                    <p> {user.role}</p>
-                </div>
-                <div className="info">
-                    <p> <b>First Name </b></p>
-                    <p> {user.firstName}</p>
-                </div>
-                <div className="info">
-                    <p> <b>Last Name </b></p>
-                    <p> {user.lastName}</p>
-                </div>
-                <div className="info">
-                    <p> <b>Email </b></p>
-                    <p> {user.email}</p>
-                </div>
-                <div className="info">
-                    <p> <b>Full Address</b></p>
-                    <p> {user.address}, {user.city}, {user.province}, Canada</p>
-                </div>
-                <div className="info">
-                    <p> <b>Phone number</b></p>
-                    <p> {user.phone}</p>
-                </div>
-                <div className="info">
-                    <p> <b>Password</b></p>
-                    <p> ********</p>
-                </div>
-                <div className="info">
-                    <p> <b>Events participated </b></p>
-                    <p> User's events</p>
-                </div>
-                <div className="info">
-                    <p> <b>Foods collected </b></p>
-                    <p> User's foods</p>
-                </div>
-                <TextField
-                    label="Interested produce"
-                    type='text'
-                    value={interest}
-                    onChange={(e) => setInterest(e.target.value)}
-                    placeholder='Carrot'
-                />
-                <Paper
-                    sx={{
-                        display: 'flex',
-                        justifyContent: 'center',
-                        flexWrap: 'wrap',
-                        listStyle: 'none',
-                        p: 0.8,
-                        m: 0
-                    }}
-                    component="ul"
-                >
-                    {chipData.map((data) => {
-                        let icon;
+                    {/* Some hardcoded values still left to update */}
+                    <div className="info">
+                        <p> <b>Account Type </b></p>
+                        <p> {user.role}</p>
+                    </div>
+                    <div className="info">
+                        <p> <b>First Name </b></p>
+                        <p> {user.firstName}</p>
+                    </div>
+                    <div className="info">
+                        <p> <b>Last Name </b></p>
+                        <p> {user.lastName}</p>
+                    </div>
+                    <div className="info">
+                        <p> <b>Email </b></p>
+                        <p> {user.email}</p>
+                    </div>
+                    <div className="info">
+                        <p> <b>Full Address</b></p>
+                        <p> {user.address}, {user.city}, {user.province}, Canada</p>
+                    </div>
+                    <div className="info">
+                        <p> <b>Phone number</b></p>
+                        <p> {user.phone}</p>
+                    </div>
+                    <div className="info">
+                        <p> <b>Password</b></p>
+                        <p> ********</p>
+                    </div>
+                    <div className="info">
+                        <p> <b>Events participated </b></p>
+                        <p> User's events</p>
+                    </div>
+                    <div className="info">
+                        <p> <b>Foods collected </b></p>
+                        <p> User's foods</p>
+                    </div>
+                    <TextField
+                        label="Interested produce"
+                        type='text'
+                        value={interest}
+                        onChange={(e) => setInterest(e.target.value)}
+                        placeholder='Carrot'
+                    />
+                    <Paper
+                        sx={{
+                            display: 'flex',
+                            justifyContent: 'center',
+                            flexWrap: 'wrap',
+                            listStyle: 'none',
+                            p: 0.8,
+                            m: 0
+                        }}
+                        component="ul"
+                    >
+                        {chipData.map((data) => {
+                            let icon;
 
-                        return (
-                        <ListItem key={data.key}>
-                            <Chip
-                            icon={icon}
-                            label={data.label}
-                            onDelete={handleDelete(data)}
-                            />
-                        </ListItem>
-                        );
-                    })}
-                </Paper>
-                <br/>
-                {/* This button is not handled yet. Would allow the user to modify their info */}
-                <Button
-                    variant={"contained"}
-                    color="success"
-                    className="modify__btn">
-                    Modify Profile
-                </Button>
+                            return (
+                            <ListItem key={data.key}>
+                                <Chip
+                                icon={icon}
+                                label={data.label}
+                                onDelete={handleDelete(data)}
+                                />
+                            </ListItem>
+                            );
+                        })}
+                    </Paper>
+                    <br/>
+                    {/* This button is not handled yet. Would allow the user to modify their info */}
+                    <Button
+                        variant={"contained"}
+                        color="success"
+                        className="modify__btn">
+                        Modify Profile
+                    </Button>
+                </div>
             </div>
-        </div>
         </div>
     </div>);
 }
