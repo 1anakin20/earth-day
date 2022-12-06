0e4e30df1d232d4b100e502f5664efe5fdd271ba
Timothy Chan <timothy.chan2@mail.mcgill.ca>
2022-12-06 00:31:29 -0500

    Remove the code that has been moved into the ProfileForm component

--- frontend/src/components/profile/profile.js
+++ frontend/src/components/profile/profile.js
@@ -10,9 +10,9 @@ import { useState } from "react";
 import defaultProfilePic from '../../images/avatar/Default_pfp.png';
 import { FaArrowLeft } from 'react-icons/fa';
 import { useNavigate } from "react-router-dom";
-import ResponsiveAppBar from "../ResponsiveAppBar";
 
-import { updateFarmerData } from "../../database/database";
+import ResponsiveAppBar from "../ResponsiveAppBar";
+import ProfileForm from "../ProfileForm";
 
 function Profile(props) {
     const { user, setUser } = props;
@@ -65,36 +65,6 @@ function Profile(props) {
         } else {
             setEditButtonText('Edit Profile');
         }
-    };
-
-    const updateUser = (event, key) => {
-        setUser(prev => ({ ...prev,
-            [key]: event.target.value
-        }));
-    };
-
-    const updateProfile = () => {
-        // Check required fields are filled
-        // Save user info to the database
-        if (user.role === 'Farmer') {
-            updateFarmerData(
-                user.id,
-                user.farmName,
-                user.firstName,
-                user.lastName,
-                user.address,
-                user.city,
-                user.province,
-                user.email,
-                user.phone,
-                user.availability,
-                user.password,
-                user.capacity,
-                user.foodBank
-            );
-        }
-        setProfileMode('');
-        setEditButtonText('Edit Profile');
     };
 
     return (
@@ -230,105 +200,14 @@ function Profile(props) {
                                 </Button>
                             </>
                         ) : (
-                            <form className="form__info">
-                                {user.role === 'Farmer' && (
-                                  
-                                    <TextField
-                                        label="Farm Name"
-                                        type='text'
-                                        value={user.farmName}
-                                        onChange={(e) => updateUser(e, "farmName")}
-                                        placeholder='Plentiful Farm'
-                                    />
-                                )}
-                                <TextField
-                                    label="First Name"
-                                    type='text'
-                                    value={user.firstName}
-                                    onChange={(e) => updateUser(e, "firstName")}
-                                    placeholder='John'
-                                />
-                                <TextField
-                                    label="Last Name"
-                                    type='text'
-                                    value={user.lastName}
-                                    onChange={(e) => updateUser(e, "lastName")}
-                                    placeholder='Smith'
-                                />
-                                <TextField
-                                    label="Address"
-                                    type='text'
-                                    value={user.address}
-                                    onChange={(e) => updateUser(e, "address")}
-                                    placeholder='5555 First Street'
-                                />
-                                <TextField
-                                    label="City"
-                                    type='text'
-                                    value={user.city}
-                                    onChange={(e) => updateUser(e, "city")}
-                                    placeholder='Montreal'
-                                />
-                                <TextField
-                                    label="Province"
-                                    type='text'
-                                    value={user.province}
-                                    onChange={(e) => updateUser(e, "province")}
-                                    placeholder='Quebec'
-                                />
-                                <TextField
-                                    label="Email"
-                                    type='email'
-                                    value={user.email}
-                                    onChange={(e) => updateUser(e, "email")}
-                                    placeholder='john.smith@email.ca'
-                                />
-                                <TextField
-                                    label="Phone Number"
-                                    type='text'
-                                    value={user.phone}
-                                    onChange={(e) => updateUser(e, "phone")}
-                                    placeholder='1-514-999-9999'
-                                />
-                                <TextField
-                                    label="Availability"
-                                    type='text'
-                                    value={user.availability}
-                                    onChange={(e) => updateUser(e, "availability")}
-                                    placeholder='Aug 5 - Aug 8 from 9 AM to 5 PM'
-                                />
-                                <TextField
-                                    label="Password"
-                                    type='password'
-                                    value={user.password}
-                                    onChange={(e) => updateUser(e, "password")}
-                                />
-                                {(user.role === 'Farmer' || user.role === 'Food Bank') && (
-                                    <TextField
-                                        label="Capacity"
-                                        type='number'
-                                        value={user.capacity}
-                                        onChange={(e) => updateUser(e, "capacity")}
-                                        placeholder='25'
-                                    />
-                                )}
-                                {user.role === 'Farmer' && (
-                                    <TextField
-                                        label="Associated Food Bank"
-                                        type='text'
-                                        value={user.foodBank}
-                                        onChange={(e) => updateUser(e, "foodBank")}
-                                        placeholder='Donations Welcome Food Bank'
-                                    />
-                                )}
-                                <Button
-                                    variant={"contained"}
-                                    color="success"
-                                    className="modify__btn"
-                                    onClick={updateProfile}>
-                                    {editButtonText}
-                                </Button>
-                            </form>
+                            <ProfileForm
+                                formType="update"
+                                user={user}
+                                submitButtonText={editButtonText}
+                                setUser={setUser}
+                                setProfileMode={setProfileMode}
+                                setSubmitButtonText={setEditButtonText}
+                            />
                         )}
                     </div>
                 </div>
