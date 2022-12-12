6cc3260b214ff8afa8601d550098ab992a69d41b
Timothy Chan <timothy.chan2@mail.mcgill.ca>
2022-12-11 21:09:07 -0500

    Change the link to the login page to match that in the Register component

--- frontend/src/components/register/RegisterForm.js
+++ frontend/src/components/register/RegisterForm.js
@@ -1,3 +1,5 @@
+import ProfileForm from "../ProfileForm";
+
 import './RegisterForm.css';
 import Button from '@mui/material/Button';
 import Link from "@mui/material/Link";
@@ -5,10 +7,12 @@ import Divider from "@mui/material/Divider";
 import Chip from "@mui/material/Chip";
 import YardIcon from "@mui/icons-material/Yard";
 
-import ProfileForm from "../ProfileForm";
+import { useNavigate } from "react-router-dom";
 
 function RegisterForm(props) {
     const { user, setUser } = props;
+
+    let navigate = useNavigate();
 
     return (
         <div className={"register__background"}>
@@ -21,7 +25,18 @@ function RegisterForm(props) {
                         </Link>
                     </h1>
                     <p>Already have an account?</p>
-                    <Link href="login" underline="none">Login</Link>
+                    <Button
+                        variant={"contained"}
+                        color={"info"}
+                        onClick={() => navigate('/login')}>
+                        Login
+                    </Button>
+                    <Button
+                        variant={"contained"}
+                        color={"warning"}
+                        onClick={() => setUser({})}>
+                        Back
+                    </Button>
                     <Divider variant={"middle"} className={"login__separation"}>
                         <Chip label={"Or register"} color={"default"}/>
                     </Divider>
@@ -36,12 +51,6 @@ function RegisterForm(props) {
                         By clicking Register, you agree to the Gleanful <br/><a href='legal'>User Agreement</a>,{' '}
                         <a href='legal'>Privacy Policy</a>, and <a href='legal'>Cookie Policy</a>.
                     </p>
-                    <Button
-                        variant={"contained"}
-                        color={"warning"}
-                        onClick={() => setUser({})}>
-                        Back
-                    </Button>
                 </section>
             </div>
         </div>
